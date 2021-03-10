330459ed3ecfa76b1ff5e6da99c4c6dbd2482e8f
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-03-10 13:49:31 +0530

    main.py added

---
+++ main.py
@@ -0,0 +1,44 @@
+import re
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from services import *
+
+print("Enter the number to be bombarded:")
+phoneNumber = input()
+
+# Checking whether 10 digits phone number or not
+if not re.match(r"^[0-9]{10}$", phoneNumber):
+    print("Enter a valid phone number")
+    exit(0)
+
+# Configuring headless mode
+options = Options()
+options.headless = True
+
+# Change the path to webdriver if needed
+browser = webdriver.Chrome(
+    options=options, executable_path=f"{os.getcwd()}/chromedriver"
+)
+
+# Flipkart Bomber
+print("Starting Flipkart Bomber")
+flipkart.flipkartBomber(browser, phoneNumber)
+print("Ending Flipkart Bomber")
+
+# Amazon Bomber
+print("Starting Amazon Bomber")
+amazon.amazonBomber(browser, phoneNumber)
+print("Ending Amazon Bomber")
+
+# Oyo Bomber
+print("Starting OYO Bomber")
+oyo.oyoBomber(browser, phoneNumber)
+print("Ending OYO Bomber")
+
+# Uber Bomber
+print("Starting Uber Bomber")
+uber.uberBomber(browser, phoneNumber)
+print("Ending Uber Bomber")
